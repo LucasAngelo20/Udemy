84a1ed52ea481e3cdd438bfcde074bf89a5244cc
Lucas Angelo <lucasg258@gmail.com>
2021-03-15 14:00:16 -0300

    Renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
 
-import Diferenciar from './components/Diferenciar';
+import ParImpar from './components/ParImpar'
+// import Diferenciar from './components/Diferenciar';
 // import ContadorV2 from './components/contador/ContadorV2'
 // import Pai from './components/indireta/Pai'
 // import Pai from './components/direta/Pai'
@@ -14,29 +15,33 @@ import Diferenciar from './components/Diferenciar';
 // import CompPadrao, {Comp1, Comp2} from './components/Multi';
 
 export default () => (
-  <View style={styles.App}>
+  <SafeAreaView style={styles.App}>
+
+    <ParImpar num={3} />
+
+    {/*
+    <MinMax min={3} max={20} />
     <Diferenciar />
-    {/*<MinMax min={3} max={20} />
     <ContadorV2 />
     <Pai />
     <Pai />
-      <Contador inicial={100} passo={13} />
-      <Contador />
-      <Botao />
-      <Titulo principal='Cadastro Produto' secundario='Tela de cadastro do produto'/>
+    <Contador inicial={100} passo={13} />
+    <Contador />
+    <Botao />
+    <Titulo principal='Cadastro Produto' secundario='Tela de cadastro do produto'/>
     <MinMax min={1} max={94} />
-
-       <Aleatorio min={50} max={2} />
-        <Aleatorio min={50} max={2} />
-        <Aleatorio min={50} max={2} />
-        <Aleatorio min={50} max={2} />
-<Aleatorio min={50} max={2} />
-
-     <CompPadrao />
+    <Aleatorio min={50} max={2} />
+    <Aleatorio min={50} max={2} />
+    <Aleatorio min={50} max={2} />
+    <Aleatorio min={50} max={2} />
+    <Aleatorio min={50} max={2} />
+    <CompPadrao />
     <Comp1 />
     <Comp2 />
-  <Primeiro /> */}
-  </View>
+    <Primeiro />
+     */}
+
+  </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
